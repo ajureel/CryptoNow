7b47bbefe095f2086c13ca97a0cc6b58ca9e5faa
MI-Optimist <ajureelmi@gmail.com>
2022-01-12 08:47:27 -0500

    test blockchain api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 // Elements
+var apiTestEl = document.getElementById('apiTest');
 
 
 // Global Vars
 
 
 // Global Constants
-const blockchainKy = "";
+const blockchainKy = ""; //key may take 2-3 days.  There are some apis that do not require authentication
 const coincapKy = "";
 
-// https://api.blockchain.com/v3/exchange
 
-//https://api.blockchain.com/v3/exchange/tickers
+// ************ API Connections **********
+// Base URL:    https://api.blockchain.com/v3/exchange
+// Ticker URL:  https://api.blockchain.com/v3/exchange/tickers
 
 var getTicker = function() {
     //this blockchain ticker does not require authentication/key
@@ -23,6 +25,24 @@ var getTicker = function() {
       .then(function (data) {
         //Using console.log to examine the data
         console.log(data);
+
+        apiTestEl
+
+        var currentHeaderEl = document.createElement('h');
+        var currentSymbolEl = document.createElement('p');
+        var currentLastTradePriceEl = document.createElement('p');
+        var currentPrice_24hEl = document.createElement('p');
+        var currentVolumeEl = document.createElement('p');
+                
+        currentHeaderEl.innerText = "Blockchain API Test";
+        currentSymbolEl.innerText = "Symbol: " + data[0].symbol;
+        currentLastTradePriceEl.innerText = "Last Price: " + data[0].last_trade_price;
+        currentPrice_24hEl.innerText = "Humidity: " + data[0].price_24h;
+        currentVolumeEl.innerText = "UV Index: " +  data[0].volume_24h;
+
+        apiTestEl.append(currentHeaderEl, currentSymbolEl, currentLastTradePriceEl, currentLastTradePriceEl, currentPrice_24hEl, currentVolumeEl);
+
+
        });
   }
 
