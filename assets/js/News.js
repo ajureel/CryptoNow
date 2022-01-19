71f06107680d2cfe3ab116db45672304d64e6b00
MI-Optimist <ajureelmi@gmail.com>
2022-01-19 18:42:58 -0500

    News Works - Collab with Shameka and Vilson

--- assets/js/News.js
+++ assets/js/News.js
@@ -15,48 +15,52 @@ var getNews = function (myKeyword) {
              //Using console.log to examine the data
               console.log(data);
               console.log(data.data[0].author);
-        
-              // Create the elements of the card
-              var myArticleEl = document.createElement('article');
-              var myAEl = document.createElement('a'); //needs href set to data[i].url
-              var myImgDivEl = document.createElement('div');
-              var myImgEl = document.createElement('img'); //src set to data[i].image
-              var myFieldsDivEl = document.createElement('div');
-              var myTitleEl = document.createElement('h1'); //.title
-              var mySourceEl = document.createElement('h2');//.source
-              var myDLEl = document.createElement('dl');
-              var myDtEl = document.createElement('dt');//"Author"
-              var myDDEl = document.createElement('dd');//.author
+			  console.log(data.data.length);
 
-        
-            console.log(data.data[1].author);
+			  for (i = 0; i < data.data.length; i++) {
+
+				  // Create the elements of the card
+				  var myArticleEl = document.createElement('article');
+				  var myAEl = document.createElement('a'); //needs href set to data[i].url
+				  var myImgDivEl = document.createElement('div');
+				  var myImgEl = document.createElement('img'); //src set to data[i].image
+				  var myFieldsDivEl = document.createElement('div');
+				  var myTitleEl = document.createElement('h1'); //.title
+				  var mySourceEl = document.createElement('h2');//.source
+				  var myDLEl = document.createElement('dl');
+				  var myDtEl = document.createElement('dt');//"Author"
+				  var myDDEl = document.createElement('dd');//.author
 
 
-            //   // Set the element attributes
-              myAEl.setAttribute('class', 'link dt w-100 bb b--black-10 pb2 mt2 dim blue');
-              myAEl.setAttribute('href', data.data[0].url);
-              myImgDivEl.setAttribute('class', 'dtc w3');
-              myImgEl.setAttribute('class', 'db w-100');
-              myImgEl.setAttribute('src',data.data[0].image);
-              myFieldsDivEl.setAttribute('class', 'dtc v-top pl2');
-              myTitleEl.setAttribute('class', 'f6 f5-ns fw6 lh-title black mv0');
-              myTitleEl.innerText = data.data[0].title;
-              mySourceEl.setAttribute('class','f6 fw4 mt2 mb0 black-60');
-              mySourceEl.innerText = data.data[0].source;
-              myDLEl.setAttribute('class','mt2 f6');
-              myDtEl.setAttribute('class', 'clip');
-              myDtEl.innerText = "Author: ";
-              myDDEl.setAttribute('class', 'm10');
-              myDDEl.innerText = data.data[0].author;
+				  console.log(data.data[i].author);
 
-            // //   //Build the card
-                myDLEl.append(myDtEl, myDDEl);
-                myFieldsDivEl.append(myTitleEl, mySourceEl, myDLEl);
-                myImgDivEl.append(myImgEl);
-                myAEl.append(myImgEl, myFieldsDivEl);
-                myArticleEl.append(myAEl);
-        
-                EverythingCryptoE1.append(myArticleEl);
+
+				  //   // Set the element attributes
+				  myAEl.setAttribute('class', 'link dt w-100 bb b--black-10 pb2 mt2 dim blue');
+				  myAEl.setAttribute('href', data.data[i].url);
+				  myImgDivEl.setAttribute('class', 'dtc w3');
+				  myImgEl.setAttribute('class', 'db w-100');
+				  //myImgEl.setAttribute('src',data.data[0].image);
+				  myFieldsDivEl.setAttribute('class', 'dtc v-top pl2');
+				  myTitleEl.setAttribute('class', 'f6 f5-ns fw6 lh-title black mv0');
+				  myTitleEl.innerText = data.data[i].title;
+				  mySourceEl.setAttribute('class', 'f6 fw4 mt2 mb0 black-60');
+				  mySourceEl.innerText = data.data[i].source;
+				  myDLEl.setAttribute('class', 'mt2 f6');
+				  myDtEl.setAttribute('class', 'clip');
+				  myDtEl.innerText = "Author: ";
+				  myDDEl.setAttribute('class', 'm10');
+				  myDDEl.innerText = data.data[i].author;
+
+				  // //   //Build the card
+				  myDLEl.append(myDtEl, myDDEl);
+				  myFieldsDivEl.append(myTitleEl, mySourceEl, myDLEl);
+				  //myImgDivEl.append(myImgEl);
+				  myAEl.append(myFieldsDivEl);
+				  myArticleEl.append(myAEl);
+
+				  EverythingCryptoE1.append(myArticleEl);
+			  }//end for loop
             });
         } else {
           alert("Error!");
@@ -65,8 +69,8 @@ var getNews = function (myKeyword) {
   
   };
   
-  //getNews('non fungible tokens');
-  console.log("test");
+  getNews('non fungible tokens');
+  //console.log("test");
 
 
 
